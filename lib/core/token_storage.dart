10d13cabc7a92556d45d7f420a6d19074c6c842a
zzingni <wldms001313@gmail.com>
2025-11-05 01:51:24 +0900

    feat : FCM 위한 userId 저장/조회 로직 추가

--- lib/core/token_storage.dart
+++ lib/core/token_storage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class TokenStorage {
   static const _kAccess = 'accessToken';
   static const _kRefresh = 'refreshToken';
+  static const _kUserId = 'userId';
   static const _storage = FlutterSecureStorage();
 
   static Future<void> saveTokens({
@@ -24,5 +25,15 @@ class TokenStorage {
   static Future<void> clear() async {
     await _storage.delete(key: _kAccess);
     await _storage.delete(key: _kRefresh);
+    await _storage.delete(key: _kUserId);
   }
+
+  // userId 저장/조회 ---
+  static Future<void> setUserId(String id) async {
+    await _storage.write(key: _kUserId, value: id);
+  }
+
+  static Future<String?> get userId async =>
+      await _storage.read(key: _kUserId);
 }
+
